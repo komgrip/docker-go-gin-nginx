a5f373c45c64617b9fab34e83e11c7d5aef926df
phanuwit.h <phanuwit.h@gmail.com>
2021-06-25 09:46:12 +0700

    example: export pdf

--- php/src/index.php
+++ php/src/index.php
@@ -1,2 +1,42 @@
 <?php
-echo json_encode(["result" => "Hello World !!! (php)"]);
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PHPJasper\PHPJasper;
+
+$outputTemp = "temp/" . date('YmdHis') . "/";
+
+$jasper = new PHPJasper;
+$input = './jasper_test.jrxml';
+$targetReportFile = './pdf';
+
+$db_connection = [
+    'driver' => 'mysql',
+    'username' => "test",
+    'password' => "123456789",
+    'host' => "192.168.0.102",
+    'database' => "test",
+];
+
+$options = [
+    'format' => ['pdf'],
+    'params' => [
+        'p_value' => "test",
+    ],
+    'db_connection' => $db_connection,
+];
+
+$jasper->process(
+    $input,
+    $targetReportFile,
+    $options
+)->execute();
+
+if (file_exists($targetReportFile . '.pdf')) {
+    header("Content-Type: application/octet-stream");
+    header('Content-Disposition: attachment; filename="downloaded.pdf"');
+    header("Content-Length: " . filesize("download.pdf"));
+    readfile($targetReportFile . '.pdf');
+    die();
+} else {
+    die("Error: File not found.");
+}
